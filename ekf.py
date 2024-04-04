fc247bfb2f71f4b14fa3ec6c73c0c8c479d8c6ca
qhdwight <qhdwight@gmail.com>
2024-04-04 17:39:44 -0400

    Predict steP

--- ekf.py
+++ ekf.py
@@ -5,8 +5,8 @@ from numpy.typing import NDArray
 
 class EKF:
     class State:
-        a: NDArray
-        q: SO3
+        a: NDArray  # World frame acceleration
+        q: SO3  # World frame orientation
         w: SO3Tangent
         theta: NDArray
         theta_dot: NDArray
@@ -16,5 +16,15 @@ class EKF:
         accelerations: NDArray
         gyroscope: NDArray
 
-    def __init__(self):
+    def __init__(self, command_mix: float) -> None:
         self.state = self.State()
+        self.command_mix = command_mix
+
+    def predict(self, u: NDArray, dt: float) -> None:
+        """
+        :param u:   Commanded joint velocities
+        :param dt:  Time step
+        """
+        self.state.q = self.state.q + SO3Tangent.exp(self.state.w * dt)
+        self.state.theta = self.state.theta + self.state.theta_dot * dt
+        self.state.theta_dot = (1 - self.command_mix) * self.state.theta_dot + self.command_mix * u

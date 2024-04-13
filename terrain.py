2ac6c078520b01c89f53307c8df59e9b9cf485ae
qhdwight <qhdwight@gmail.com>
2024-04-13 16:56:07 -0400

    Remove stupid pca

--- terrain.py
+++ terrain.py
@@ -43,26 +43,3 @@ class Terrain:
                 self.normals[x, y] = hit_normal
                 point = np.array([x_c, y_c, self.heightmap[x, y]])
                 client.addUserDebugLine(point, point + hit_normal, [1, 0, 0])
-                # # Grab nearby points
-                # nearby_points = []
-                # for dx in [-1, 0, 1]:
-                #     for dy in [-1, 0, 1]:
-                #         if 0 <= x + dx < cols and 0 <= y + dy < rows:
-                #             nearby_points.append([x - rows / 2 + dx, y - cols / 2 + dy, self.heightmap[x + dx, y + dy]])
-                # # Compute normal via PCA
-                # nearby_points = np.array(nearby_points)
-                # nearby_points = nearby_points - np.mean(nearby_points, axis=0)
-                # cov = np.cov(nearby_points, rowvar=False)
-                # _, (normal, _, _) = np.linalg.eigh(cov)
-                # normal /= np.linalg.norm(normal)
-                # # Flip if facing downward
-                # if normal[2] < 0:
-                #     normal *= -1
-                #
-                # self.normals[x, y] = normal
-                #
-                # print(normal)
-                # print(nearby_points)
-                # p_in_map = np.array([x, y, self.heightmap[x, y]])
-                # p_in_world = p_in_map - np.array([rows / 2, cols / 2, 0])
-                # client.addUserDebugLine(p_in_world, p_in_world + normal, [1, 0, 0])

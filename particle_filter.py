e7907271321021c75bcd9cb1b0ae0fc2dc724fbc
qhdwight <qhdwight@gmail.com>
2024-04-15 17:08:46 -0400

    PRediction not crashing anymore

--- particle_filter.py
+++ particle_filter.py
@@ -2,7 +2,7 @@ from dataclasses import dataclass, field
 
 import numpy as np
 from manifpy import SE3, SO3, SE3Tangent
-from numpy.typing import NDArray
+from numpy.typing import NDArray, ArrayLike
 
 from terrain import Terrain
 
@@ -20,15 +20,25 @@ class TerrainParticleFilter:
     def likelihood(self, particle: Particle, joint_contact_normals_in_world: NDArray):
         pass
 
-    def M(self, u: SE3Tangent) -> NDArray:
-        pass
+    def M(self, u: SE3Tangent, alphas: ArrayLike) -> NDArray:
+        # a0, a1, a2, a3, a4, a5 = alphas
+        # p1, p2, p3, t1, t2, t3 = u.coeffs()
+        # return np.array([[a0 * p1 ** 2 + a1 * p2 ** 2, 0, 0],
+        #                  [0, a2 * p1 ** 2 + a3 * p2 ** 2, 0],
+        #                  [0, 0, a4 * p1 ** 2 + a5 * p2 ** 2]])
+        return np.eye(6) * 0.1
 
     def prediction(self, ekf_orientation: SO3, commanded_twist: SE3Tangent):
-        M = self.M(commanded_twist)
+        if commanded_twist.squaredWeightedNorm() < 1e-6:
+            return
+
+        M = self.M(commanded_twist, None)
         LQ = np.linalg.cholesky(M)
+
         for particle in self.particles:
-            u_sample = commanded_twist + np.random.normal(0, 1, 6) @ LQ
+            u_sample = commanded_twist + SE3Tangent(np.random.normal(0, 1, 6) @ LQ)
             particle.head_link = particle.head_link + u_sample
 
-    def correction(self, joint_contact_normals_in_world: NDArray):
-        pass
+
+def correction(self, joint_contact_normals_in_world: NDArray):
+    pass
